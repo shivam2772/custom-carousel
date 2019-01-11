e5489b1d6e2246034c8bb732f247b6fbeae4440d
Shivam kumar <skhandelwal8@sapient.com>
2019-01-11 15:29:19 +0530

    passed carousel width as prop

--- src/App.js
+++ src/App.js
@@ -1,91 +1,16 @@
 import React, { Component } from 'react';
+import { css } from 'emotion';
 import './App.css';
 import Carousel from './carousel';
 import classNames from 'classnames';
 import Aus from './assets/img/australia.jpg';
 
-const images = [
-  {
-    src: './assets/img/australia.jpg',
-    imgName: 'australia.jpg',
-    name: 'Australia'
-  },
-  {
-    src: './assets/img/istanbul.jpg',
-    imgName: 'australia.jpg',
-    name: 'Istanbul'
-  },
-  {
-    src: './assets/img/marina_bay_sands.jpg',
-    imgName: 'australia.jpg',
-    name: 'Marina'
-  },
-  {
-    src: "'./assets/img/santorini.jpg'",
-    imgName: 'australia.jpg',
-    name: 'Santorini'
-  },
-  {
-    src: './assets/img/turkey.jpg',
-    imgName: 'australia.jpg',
-    name: 'Turkey'
-  }
-];
 class App extends Component {
-  renderContent = () => {
-   return (
-    images.map(item => {
-      const img = require(`./assets/img/${item.imgName}`);
-      console.log(img.toString());
-      return(
-        <div className={classNames('Slick-content')}>
-          <img src={require('./assets/img/australia.jpg')} alt="demo" style={{ width: '100%'}}/>
-          <span>""</span>
-          <div className="image-text-content">
-            <div className="FormDiv">
-              <div className="InputDiv">
-                <label>Destination</label>
-                <input/>
-              </div>
-              <div className="InputDiv">
-                <label>Destination</label>
-                <input/>
-              </div>
-              <div className="InputDiv">
-                <label>Destination</label>
-                <input/>
-              </div>
-            </div>
-            <span>Find adventure of lifetime</span>
-          </div>
-        </div>
-      )
-    })
-   );
-  }
+
   render() {
     return (
       <div className="App">
-        <Carousel>
-          {this.renderContent()}
-          {/* <div className="Slick-content">
-            <h3>1</h3>
-          </div>
-          <div className="Slick-content">
-            <h3>2</h3>
-          </div>
-          <div className="Slick-content">
-            <h3>3</h3>
-          </div>
-          <div className="Slick-content">
-            <h3>4</h3>
-          </div>
-          <div className="Slick-content">
-            <h3>5</h3>
-          </div>
-          <div className="Slick-content">
-            <h3>6</h3>
-          </div> */}
+        <Carousel height='90vh' width='400px'>
       </Carousel>
       </div>
     );
--- src/carousel.js
+++ src/carousel.js
@@ -1,30 +1,97 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Slider from 'react-slick';
-import elements from './App';
+import classNames from 'classnames';
 
 const settings = {
     dots: true,
-    infinite: true,
+    infinite: false,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1
 };
 
-class Carousel extends React.PureComponent {
-    constructor(props) {
-        super(props);
+const images = [
+    {
+      src: './assets/img/australia.jpg',
+      imgName: 'australia.jpg',
+      name: 'Australia'
+    },
+    {
+      src: './assets/img/istanbul.jpg',
+      imgName: 'istanbul.jpg',
+      name: 'Istanbul'
+    },
+    {
+      src: './assets/img/marina_bay_sands.jpg',
+      imgName: 'marina_bay_sands.jpg',
+      name: 'Marina'
+    },
+    {
+      src: "'./assets/img/santorini.jpg'",
+      imgName: 'santorini.jpg',
+      name: 'Santorini'
+    },
+    {
+      src: './assets/img/turkey.jpg',
+      imgName: 'turkey.jpg',
+      name: 'Turkey'
     }
+  ];
 
+
+
+class Carousel extends React.PureComponent {
+    renderContent = height => {
+        return (
+         images.map(item => {
+           const img = require(`./assets/img/${item.imgName}`);
+           console.log(img.toString());
+           return(
+             <div className='Slick-content' style={{height}}>
+               <img src={require(`./assets/img/${item.imgName}`)} alt="demo" style={{ width: '100%', height}}/>
+               <div className="image-text-content">
+                 <div className="FormDiv">
+                   <div className="InputDiv">
+                     <label>Destination</label>
+                     <input/>
+                   </div>
+                   <div className="InputDiv">
+                     <label>Destination</label>
+                     <input/>
+                   </div>
+                   <div className="InputDiv">
+                     <label>Destination</label>
+                     <input/>
+                   </div>
+                 </div>
+                 <span className={classNames('text-carousel-subheading')}>Find adventure of lifetime </span>
+               </div>
+             </div>
+           )
+         })
+        );
+       }
     render() {
-        console.log('1111: ', elements);
+        console.log('111: ', this.props);
         return(
             <div style={{width: this.props.width || '100%', height: this.props.height || '60vh' }}>
                 <Slider {...settings}>
-                    {this.props.children}
+                    {this.renderContent(this.props.height || '60vh')}
                 </Slider>
             </div>
-        )
+        );
     }
 }
+
+Carousel.defaultProps = {
+    height: '40vh',
+    width: '100%'
+};
+
+Carousel.propTypes = {
+    height: PropTypes.string.isRequired,
+    width: PropTypes.string
+};
 
 export default Carousel;
